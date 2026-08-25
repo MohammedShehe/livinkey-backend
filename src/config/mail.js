6d89aa22bf53019759b2de00b2c3efc4a700840a
Mohammedsnake <mosnake111@gmail.com>
2026-08-25 18:34:45 +0530

    Added ConnectionTimeout functions on mail config

--- src/config/mail.js
+++ src/config/mail.js
@@ -8,7 +8,11 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    
+    connectionTimeout: 5000,
+    greetingTimeout: 5000,
+    socketTimeout: 10000,
 });
 
 module.exports = transporter;
